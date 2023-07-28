4681fcd56d47654c087ab97d7d29a0ab5680ffe1
SkyZeroZx <73321943+SkyZeroZx@users.noreply.github.com>
2023-07-27 19:09:19 -0500

    chore: update release.config

--- release.config.js
+++ release.config.js
@@ -26,7 +26,7 @@ module.exports = {
     [
       '@semantic-release/exec',
       {
-        prepareCmd: `VERSION=\${nextRelease.version} npx nx run-many -t release && VERSION=\${nextRelease.version} npx -p replace-json-property rjp ./package.json version \${nextRelease.version}`,
+        prepareCmd: `VERSION=\${nextRelease.version} npx nx run-many -t release && VERSION=\${nextRelease.version} npm run bump-version:root`,
       },
     ],
     [
